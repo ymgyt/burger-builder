481afb58eb01bf8c3a2e99e6742683aef5573a15
ymgyt <yamaguchi7073xtt@gmail.com>
2019-10-08 02:29:37 +0900

    curse 190

--- frontend/src/hoc/Layout/Layout.js
+++ frontend/src/hoc/Layout/Layout.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Aux';
+import Aux from '../Aux/Aux';
 import classes from './Layout.module.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
-class Layout extends React.Component {
+class Layout extends Component {
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     };
 
     sideDrawerClosedHandler = () => {
         this.setState({ showSideDrawer: false });
     }
 
+    sideDrawerToggleHandler = (prevState) => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    }
+
     render() {
         return (
             <Aux>
-                <Toolbar />
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
                 <SideDrawer
                     open={this.state.showSideDrawer}
                     closed={this.sideDrawerClosedHandler} />
@@ -25,7 +31,6 @@ class Layout extends React.Component {
                     {this.props.children}
                 </main>
             </Aux>
-
         )
     }
 }
